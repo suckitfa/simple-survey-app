104d4bd17105ef7f853c50bb627cdf66ad6d5fc8
Guangyuan <254202042@qq.com>
2023-10-15 17:03:12 +0800

    feat: 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,20 @@
-const NotFound = () => {
-    return <p>not found</p>
-}
+import { Result, Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
-export default NotFound
+const NotFound = () => {
+    const nav = useNavigate()
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="未找到对应页面"
+      extra={
+        <Button type="primary" onClick={() => nav("/")}>
+          首页
+        </Button>
+      }
+    />
+  );
+};
+
+export default NotFound;
